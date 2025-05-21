c8d615e7a696673241f0309c0b8f861f9194e586
Erik Pohl <erik.pohl.444@gmail.com>
2025-05-20 20:34:09 -0400

    added alerts to UseState component

--- src/components/UseStateComponent.tsx
+++ src/components/UseStateComponent.tsx
@@ -4,10 +4,14 @@ export function UseStateComponent() {
   const [count, setCount] = useState(0);
 
   const increment = () => {
+    alert("Increment button clicked, activating onClick event in html, which kicked off the increment function");
     setCount(count + 1);
+    alert("setCount function called, which is part of the UseState definition and which updates the count state variable");
   };
   const decrement = () => {
+    alert("Decrement button clicked, activating onClick event in html, which kicked off the increment function");
     setCount(count - 1);
+    alert("setCount function called, which is part of the UseState definition and which updates the count state variable");
   };
 
   return (
